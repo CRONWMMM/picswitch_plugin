ea7ac4946cf81205ded7fe1aa80652cd010a2158
CRONWMMM <kimmmiy@sina.cn>
2017-03-05 14:23:49 +0800

    完善JS，添加了enable和build方法

--- demo/script/picswitch.js
+++ demo/script/picswitch.js
@@ -24,6 +24,7 @@
 			// 定义一个album属性
 			// 用于存放所有相同组别的图片对象信息
 			this.album = [];
+			this.group = void 0;
 			this.currentImageIndex = void 0;
 			// 调用init方法进行初始化
 			this.init();
@@ -50,29 +51,101 @@
 				var self = this;
 				// enable和build方法必须要等到DOM加载完成才能执行
 				$(function(){
-					self.enable();
 					self.build();
+					self.enable();
 				});
 			},
 
 
 			enable : function(){
 				var self = this;
-				alert($("img").length);
+				$("body").on('click','a[data-picswitch]',function(e){
+					e.stopPropagation();	// 阻止冒泡
+					e.preventDefault();		// 阻止默认行为
+					self.start($(this));
+				});
 			},
 
 
+			// 构建渲染DOM文档
+			// 缓存相应的jQuery对象
 			build : function(){
-				var self = this;
-				alert($("#container").length);
+				var self      = this,
+					mask      = $('<div id="mask"></div>'),	// 遮罩层				
+					picswitch = $('<div id="picswitch">' + 	// 图片弹框
+										'<span class="pic-close"></span>' + 
+										'<div class="pic-view">' + 
+											'<span class="pic-btn pic-btn-prev"></span>' + 
+											'<img src="" alt="">' + 
+											'<span class="pic-btn pic-btn-next"></span>' + 
+										'</div>' + 
+										'<div class="pic-caption">' + 
+											'<p class="pic-des">This is picture describtion.</p>' + 
+											'<span class="pic-index">0 / 0</span>' + 
+										'</div>' + 
+									'</div>');
+				// 将渲染好的DOM结构插入到文档中
+				$(document.body).append(mask,picswitch);
+
+				// 缓存相应的jQuery对象
+				this.$mask      = $("#mask");							// 遮罩层对象
+				this.$picswitch = $("#picswitch");						// 图片弹框对象
+				this.$close     = $("#picswitch .pic-close");			// 关闭图标对象
+				this.$view      = $("#picswitch .pic-view");			// 预览区对象
+				this.$prevbtn   = $("#picswitch .pic-btn-prev");		// 向前切换按钮对象
+				this.$nextbtn   = $("#picswitch .pic-btn-next");		// 向后切换按钮对象
+				this.$img       = $("#picswitch img");					// 展示图片对象
+				this.$caption   = $("#picswitch .pic-caption");			// 信息展示对象
+				this.$des       = $("#picswitch .pic-des");				// 描述对象
+				this.$index     = $("#picswitch .pic-index");			// Index对象
+			},
+
+
+			// 显示遮罩层和图片弹框
+			// 如果是图像组就将图像组里的所有图片信息添加到相册数组中
+			// @param  $this参数为当前点击的对象
+			start : function($this){
+				var self  = this,
+					currentgroup = $this.attr("data-picswitch");
+				fillAlbum(currentgroup);
+				initPop();
+				
+
+				/**
+				 * fillAlbum 函数用于获得图片集里所有图片信息，并保存到album中
+				 * @param  groupName 参数为当前点击图片所在的图片组名
+				 */
+				function fillAlbum(groupName){
+					if(self.group !== groupName){
+						self.group = groupName;
+						self.album = [];
+						$("*[data-picswitch=" + groupName + "]").each(function(){
+							self.album.push({
+								src : $(this).find("img").attr("data-src")
+							});
+						});
+					}
+				}
+
+				/**
+				 * initPop 函数用于初始化图片弹窗
+				 * @return {[type]} [description]
+				 */
+				function initPop(){
+
+				}
+
 			}
+	
+
 		}
 
 
 		PicSwitch.defaults = {
 			albumLabel        : '0 / 0',							// 图片索引
 			fadeDuration      : 600,								// 淡入淡出渐变时间
-			imageFadeDuration : 600									// 图片淡入淡出渐变时间
+			imageFadeDuration : 600,								// 图片淡入淡出渐变时间
+
 		};
 
 		return PicSwitch;
